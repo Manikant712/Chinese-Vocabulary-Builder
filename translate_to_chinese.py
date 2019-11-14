e9bcaeca53c32f54356ee34b2d0f00a6bdf2bb88
Manikant Gautam <Manikant712@users.noreply.github.com>
2019-11-15 00:56:31 +0800

    Add files via upload

---
+++ translate_to_chinese.py
@@ -0,0 +1,22 @@
+from googletrans import Translator
+import pinyin 
+translator = Translator()
+file1 = open("words_meaning.txt","w")
+with open('words.txt') as f:
+    for line in f:
+        for word in line.split():
+            try:
+                ts = translator.translate(word)
+                pn = pinyin.get(word)
+                ws = word + ",  " + pn + ",  " + ts.text
+                file1.write(ws)
+                file1.write("\n")
+                print(word)
+            except Exception:
+                pass
+        
+    
+
+##ts = translator.translate('你好')
+##
+##print(ts.text)
